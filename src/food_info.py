e42e389d5f6c89e5602fb6ad4a048893b0a89f01
SeoungHyuckWon <66349040+SeoungHyuckWon@users.noreply.github.com>
2020-10-06 14:57:14 +0900

    Update food_info.py

--- src/food_info.py
+++ src/food_info.py
@@ -27,13 +27,17 @@ def make_db(total):
         food_info(count,count+999)
         count += 1000
     food_info(num1*1000+1,total)
+    
+try:
+    key ="40afbe2975a0467d837e"
+    url = "http://openapi.foodsafetykorea.go.kr/api/{}/I2790/json/{}/{}".format(key,1,1)
+    res = requests.get(url).json()
+    total_count = int(res['I2790']["total_count"])
+    f = open(food_info_path+food_info_name,'w')
+    f.write(str(total_count)+"\n")
+    f.close()
 
-key ="40afbe2975a0467d837e"
-url = "http://openapi.foodsafetykorea.go.kr/api/{}/I2790/json/{}/{}".format(key,1,1)
-res = requests.get(url).json()
-total_count = int(res['I2790']["total_count"])
-f = open(food_info_path+food_info_name,'w')
-f.write(str(total_count)+"\n")
-f.close()
-
-make_db(total_count)
+    make_db(total_count)
+except requests.exceptions.ConnectionError as msg:
+    print("API 호출 error 발생으로 유해식품정보 업데이트를 실패하였습니다.\n")
+    print("통신 오류입니다. 네트워크를 확인해주세요.")
