04e27b3ee3bd4f3aa2b21b0c05dbf1058237e73d
azzyjk <56161518+azzyjk@users.noreply.github.com>
2020-10-07 20:58:20 +0900

    diary, loadDiary : 입력형식 변경, 일지 불러오기 메뉴에 해당되는 항목들 loadDiary.py로 이동

--- src/diary.py
+++ src/diary.py
@@ -67,7 +67,7 @@ def loadDiary(user):
 
     # 년도 월 입력받기
     while True:
-        YYYYMM = input("불러오실 일지의 년도와 월을 입력해주세요.")
+        YYYYMM = input("불러오실 일지의 년도와 월을 입력해주세요.\n입력 > ")
         if YYYYMM == '0':
             return 
         else:
@@ -78,7 +78,7 @@ def loadDiary(user):
     # 기획서 수정 : 0입력하면 메인으로
     while True:
         showDiaryList(user, YYYYMM)
-        DD = input("불러오실 일지의 날짜를 입력해주세요.")
+        DD = input("불러오실 일지의 날짜를 입력해주세요.\n입력 > ")
         if DD == '0':
             return
         else:
@@ -99,40 +99,3 @@ def loadDiaryList(user, YYYYMMDD):
     for line in lines:
         foodList.append(line.replace('\n', ''))
     return foodList
-
-def editDiary(user, YYYYMMDD):
-    diaryFile = "./Users/"+user+"/diary/"+YYYYMMDD+".txt"
-    foodList = loadDiaryList(user, YYYYMMDD)
-    select = None
-    change = None
-
-    print("수정할 음식을 선택해주세요.")
-
-    for i in range(len(foodList)):
-        print(i, foodList[i])
-
-    while True:
-        select = input()
-        res = re.search('[0-9]', select)
-        if res != None:
-            if int(select)>=0 and int(select)<len(foodList):
-                change = input()
-                if len(change)==0:
-                    del foodList[int(select)]
-                    break
-                else:
-                    print(int(select), change)
-                    foodList[int(select)] = change
-                    break
-            else:
-                print("존재하지 않는 인덱스를 선택했습니다. 다시 입력해주세요.")
-        else:
-            print("숫자외에 문자를 입력할 수 없습니다. 다시 입력해주세요.") # 추가
-    f = open(diaryFile, 'w')
-    for i in foodList:
-        f.write(i+'\n')
-    f.close()            
-
-def delDiary(user, YYYYMMDD):
-    diaryFile = "./Users/"+user+"/diary/"+YYYYMMDD+".txt"
-    os.remove(diaryFile)
--- src/loadDiary.py
+++ src/loadDiary.py
@@ -1,5 +1,44 @@
 from menu import *
-from diary import *
+import os
+from diary import loadDiaryList
+import re
+
+def editDiary(user, YYYYMMDD):
+    diaryFile = "./Users/"+user+"/diary/"+YYYYMMDD+".txt"
+    foodList = loadDiaryList(user, YYYYMMDD)
+    select = None
+    change = None
+
+    print("수정할 음식을 선택해주세요.")
+
+    for i in range(len(foodList)):
+        print(i, foodList[i])
+
+    while True:
+        select = input( "입력 > ")
+        res = re.search('[^0-9]', select) # 숫자 이외의 것들이 있는지 확인
+        if res == None:
+            if int(select)>=0 and int(select)<len(foodList):
+                change = input()
+                if len(change)==0:
+                    del foodList[int(select)]
+                    break
+                else:
+                    print(int(select), change)
+                    foodList[int(select)] = change
+                    break
+            else:
+                print("존재하지 않는 인덱스를 선택했습니다. 다시 입력해주세요.")
+        else:
+            print("숫자 외에 문자를 입력할 수 없습니다. 다시 입력해주세요.")
+    f = open(diaryFile, 'w')
+    for i in foodList:
+        f.write(i+'\n')
+    f.close()            
+
+def delDiary(user, YYYYMMDD):
+    diaryFile = "./Users/"+user+"/diary/"+YYYYMMDD+".txt"
+    os.remove(diaryFile)
 
 def loadDiaryPrompt(user, YYYYMMDD):
     loadDiaryTask = showMenu("일지 불러오기 메뉴")
